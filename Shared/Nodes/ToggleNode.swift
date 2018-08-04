3c9046a3b2905433fef4d5c92f9e9e4d494e699e
Wolfgang Schreurs <wolfgang@wolftrail.net>
2018-08-04 11:10:27 +0700

    Code clean-up

--- Shared/Nodes/ToggleNode.swift
+++ Shared/Nodes/ToggleNode.swift
@@ -27,7 +27,7 @@ class ToggleNode: SKShapeNode, MenuNode {
         let h = height
         
         let font = try Font(name: self.label.fontName!, size: self.label.fontSize)        
-        let diff = (font.maxHeight - label.calculateAccumulatedFrame().height) / 2
+        let diff = font.maxHeight - (label.calculateAccumulatedFrame().height / 2)
 
         self.toggle = SKShapeNode(ellipseOf: CGSize(width: font.xHeight, height: font.xHeight))
         self.toggle.strokeColor = .white
@@ -40,11 +40,8 @@ class ToggleNode: SKShapeNode, MenuNode {
         let w = labelFrame.width + self.spacing + toggleFrame.width
         
         self.path = CGPath(rect: CGRect(x: 0, y: 0, width: w, height: h), transform: nil)
-        self.toggle.position = CGPoint(x: w - self.toggle.frame.width / 2, y: h / 2 + titleYOffset)
-        
-        print("ascender: \(font.ascender) descender: \(font.descender)")
-        print("diff: \(diff)")
-        self.label.position = CGPoint(x: 0, y: diff + font.maxHeight / 2 + self.titleYOffset)
+        self.toggle.position = CGPoint(x: w - self.toggle.frame.width / 2, y: h / 2 + titleYOffset)        
+        self.label.position = CGPoint(x: 0, y: diff + self.titleYOffset)
 
         addChild(self.label)
         addChild(self.toggle)
