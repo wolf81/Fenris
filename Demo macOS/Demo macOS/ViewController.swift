03cf987b5c4c819e03614f7c69ad059bb05e4adc
Wolfgang Schreurs <wolfgang@wolftrail.net>
2019-06-13 12:55:58 +0700

    Add macOS demo project

---
+++ Demo macOS/Demo macOS/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Demo macOS
+//
+//  Created by Wolfgang Schreurs on 13/06/2019.
+//  Copyright © 2019 Wolftrail. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import Fenris
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            
+            let scene = MenuScene(size: view.bounds.size, controlHeight: 40, options: [
+                Button(title: "Test", selected: { print("test ...") })
+            ])
+            scene.scaleMode = .aspectFill
+
+            // Present the scene
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
