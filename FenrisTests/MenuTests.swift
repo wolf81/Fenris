746692a9e6622a7c20b08d37f7cf6c08ec91ba99
Wolfgang Schreurs <wolfgang@wolftrail.net>
2019-06-18 22:42:00 +0700

    Refactor menu, add builders for single-column and two-column menus & update docs

--- FenrisTests/MenuTests.swift
+++ FenrisTests/MenuTests.swift
@@ -19,23 +19,17 @@ class MenuTests: XCTestCase {
     }
     
     func testMenu() {
-        let configuration = MenuBuilder.Configuration(
-            menuWidth: 0,
-            rowHeight: 0,
-            titleFont: Font(name: "Papyrus", size: 12)!,
-            labelFont: Font(name: "Papyrus", size: 10)!
-        )
-
-        let builder = MenuBuilder(configuration: configuration)
+        let menu = LabeledMenuBuilder()
             .withHeader(title: "Title")
             .withRow(title: "Row", item: NumberChooserItem(range: (0 ... 0), selectedValue: 0))
             .withFooter(items: [FixedSpaceItem(), FixedSpaceItem(), FixedSpaceItem()])
-        
-        let menu = builder.build()
-        XCTAssertTrue(menu.title == "Title")
-        XCTAssertTrue(menu.items.count == 2)
-        XCTAssertTrue(menu.items[0] is LabelItem)
-        XCTAssertTrue(menu.items[1] is NumberChooserItem)
+            .build()
+
+        let headerLabel = menu.headerItems.first as! LabelItem
+        XCTAssertTrue(headerLabel.title == "Title")
+        XCTAssertTrue(menu.listItems.first?.count == 2)
+        XCTAssertTrue(menu.listItems.first?[0] is LabelItem)
+        XCTAssertTrue(menu.listItems.first?[1] is NumberChooserItem)
         XCTAssertTrue(menu.footerItems.count == 3)
     }
 }
---
+++ Shared/Menu/MenuConfiguration.swift
@@ -0,0 +1,25 @@
+//
+//  Menu.Configuration.swift
+//  Fenris
+//
+//  Created by Wolfgang Schreurs on 15/06/2019.
+//  Copyright © 2019 Wolftrail. All rights reserved.
+//
+
+import SpriteKit
+
+/// A menu configuration contains verious properties used for display of the menu.
+public struct MenuConfiguration {
+    let menuWidth: CGFloat
+    let rowHeight: CGFloat
+    let titleFont: Font
+    let labelFont: Font
+    let focusRectColor: SKColor = .yellow
+    
+    public init(menuWidth: CGFloat, rowHeight: CGFloat, titleFont: Font, labelFont: Font) {
+        self.menuWidth = menuWidth
+        self.rowHeight = rowHeight
+        self.titleFont = titleFont
+        self.labelFont = labelFont
+    }
+}
