4b0ddfbb329cc69f1da140acbc607f37196b3374
Wolfgang Schreurs <wolfgang@wolftrail.net>
2019-06-18 13:30:17 +0700

    Add menu tests

--- FenrisTests/MenuTests.swift
+++ FenrisTests/MenuTests.swift
@@ -19,13 +19,23 @@ class MenuTests: XCTestCase {
     }
     
     func testMenu() {
-        let menu = Menu(
-            title: nil,
-            items: [
-                FixedSpaceItem(),
-            ],
-            footerItems: [],
-            configuration: MenuBuilder.Configuration(menuWidth: 0, rowHeight: 0, titleFont: Font(name: "Papyrus", size: 12)!, labelFont: Font(name: "Papyrus", size: 10)!)
+        let configuration = MenuBuilder.Configuration(
+            menuWidth: 0,
+            rowHeight: 0,
+            titleFont: Font(name: "Papyrus", size: 12)!,
+            labelFont: Font(name: "Papyrus", size: 10)!
         )
+
+        let builder = MenuBuilder(configuration: configuration)
+            .withHeader(title: "Title")
+            .withRow(title: "Row", item: NumberChooserItem(range: (0 ... 0), selectedValue: 0))
+            .withFooter(items: [FixedSpaceItem(), FixedSpaceItem(), FixedSpaceItem()])
+        
+        let menu = builder.build()
+        XCTAssertTrue(menu.title == "Title")
+        XCTAssertTrue(menu.items.count == 2)
+        XCTAssertTrue(menu.items[0] is LabelItem)
+        XCTAssertTrue(menu.items[1] is NumberChooserItem)
+        XCTAssertTrue(menu.footerItems.count == 3)
     }
 }
